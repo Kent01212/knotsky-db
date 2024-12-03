fef84fb733b5b78b76b5a09c5b9c82799f3df77a
Kent01212 <kd1244225@st.kobedenshi.ac.jp>
2024-12-03 13:51:12 +0900

    Webサーバー、DBサーバー、MinIO環境構築 (仮

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,3 +126,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MinIO
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = 'minioadmin'
+AWS_SECRET_ACCESS_KEY = 'minioadmin'
+AWS_STORAGE_BUCKET_NAME = 'my-bucket'  # 適宜変更
+AWS_S3_ENDPOINT_URL = 'http://minio_container:9000'
+AWS_QUERYSTRING_AUTH = False
